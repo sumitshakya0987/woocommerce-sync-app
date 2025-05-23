17cfe39a06971b05bcb8b3bcd762c674a5c1434b
sumitshakya0987 <shakyasumit@gmail.com>
2025-05-23 14:21:26 +0530

    Fix API endpoints for deployment and update server config

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-router.get('/my-products', productController.getMyProducts);
-router.post('/create', productController.createProduct);
+router.get('/my', productController.getMyProducts);
+router.post('/', productController.createProduct);
 
 module.exports = router;
